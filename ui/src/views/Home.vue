71dda5e4327b4ea74ddaf6961655f0c0e9ff7986
许晓东 <763795151@qq.com>
2021-09-12 22:31:08 +0800

    show config in the home page

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to kafka console ui." />
+    <a-card title="kafka console 配置" style="width: 100%">
+      <!--      <a slot="extra" href="#">more</a>-->
+      <p v-for="(v, k) in config" :key="k">{{ k }}={{ v }}</p>
+    </a-card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import request from "@/utils/request";
+import { KafkaConfigApi } from "@/utils/api";
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      config: {},
+    };
+  },
+
+  created() {
+    request({
+      url: KafkaConfigApi.getConfig.url,
+      method: KafkaConfigApi.getConfig.method,
+    }).then((res) => {
+      this.config = res.data;
+    });
   },
 };
 </script>
