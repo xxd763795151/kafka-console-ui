3ef9f1e012166a383dc01614012d89721bafe741
许晓东 <763795151@qq.com>
2023-05-22 23:17:41 +0800

    shell 脚本启停路径修复.

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname "`pwd`/$0")
+PREFIX='./'
+CMD=$0
+if [[ $CMD == $PREFIX* ]]; then
+        CMD=${CMD:2}
+fi
+SCRIPT_DIR=$(dirname "`pwd`/$CMD")
 PROJECT_DIR=`dirname "$SCRIPT_DIR"`
 # 不要修改进程标记，作为进程属性关闭使用
 PROCESS_FLAG="kafka-console-ui-process-flag:${PROJECT_DIR}"
--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,12 @@
 # 设置jvm堆大小及栈大小，栈大小最少设置为256K，不要小于这个值，比如设置为128，太小了
 JAVA_MEM_OPTS="-Xmx512m -Xms512m -Xmn256m -Xss256k"
 
-SCRIPT_DIR=$(dirname "`pwd`/$0")
+PREFIX='./'
+CMD=$0
+if [[ $CMD == $PREFIX* ]]; then
+        CMD=${CMD:2}
+fi
+SCRIPT_DIR=$(dirname "`pwd`/$CMD")
 PROJECT_DIR=`dirname "$SCRIPT_DIR"`
 CONF_FILE="$PROJECT_DIR/config/application.yml"
 TARGET="$PROJECT_DIR/lib/kafka-console-ui.jar"
@@ -12,9 +17,9 @@ TARGET="$PROJECT_DIR/lib/kafka-console-ui.jar"
 DATA_DIR=$PROJECT_DIR
 
 # 日志目录，默认为当前工程目录下
-# 这个是错误输出，如果启动命令有误，输出到这个文件，应用日志不会输出到error.out，应用日志输出到上面的rocketmq-reput.log中
+# 这个是错误输出，如果启动命令有误，输出到这个文件，应用日志不会输出到error.out，应用日志输出到上面的kafka-console-ui.log中
 ERROR_OUT="$PROJECT_DIR/error.out"
-# 不要修改进程标记，作为进程属性关闭使用，如果要修改，请把stop.sh里的该属性的值保持一致
+# 不要修改进程标记，作为进程属性关闭使用，如果要修改，请把shutdown.sh里的该属性的值保持一致
 PROCESS_FLAG="kafka-console-ui-process-flag:${PROJECT_DIR}"
 
 JAVA_OPTS="$JAVA_OPTS $JAVA_MEM_OPTS -Dfile.encoding=utf-8"
